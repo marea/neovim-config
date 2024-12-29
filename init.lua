49757391278ebd9ad2ae5049431ed8deab89280c
marea <m.arevalo.aragon@gmail.com>
2024-12-29 04:50:04 -0600

    Added keymap to close current buffer

--- init.lua
+++ init.lua
@@ -54,6 +54,7 @@ vim.keymap.set(
   '<C-\\><C-n>',
   { desc = 'Exit terminal mode' }
 )
+vim.keymap.set('n', '<leader>x', ':bd<CR>', { desc = 'Close current buffer' })
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
