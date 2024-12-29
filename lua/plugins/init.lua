eb5bb1168edb17d50d458266e8ea914f518587f7
marea <m.arevalo.aragon@gmail.com>
2024-12-29 07:46:41 -0600

    Added nvim-xresources colorscheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,27 @@
 return {
   'xiyaowong/transparent.nvim',
   { -- Colorscheme
-    'nyoom-engineering/oxocarbon.nvim',
+    'martineausimon/nvim-xresources',
+    lazy = false,
     priority = 1000,
     init = function()
-      vim.opt.background = 'dark'
-      vim.cmd.colorscheme 'oxocarbon'
-      vim.cmd.hi 'Comment gui=none'
+      require('nvim-xresources').setup {
+        --  Optional config:
+        --  xresources_path = os.getenv("HOME") .. '/.Xresources',
+        --  auto_light = {
+        --    enable = true,
+        --    value = 0.5,
+        --    exclude = {},
+        --  },
+        --  contrast = 1,
+        --  bold = true,
+        --  palette_overrides = {},
+        --  fallback_theme = "nord"
+      }
+      --local C = require('nvim-xresources.colors')
+      --require('nvim-xresources').custom_highlight_groups({ })
+
+      vim.cmd 'colorscheme xresources'
     end,
   },
   'tpope/vim-sleuth',
@@ -14,21 +29,58 @@ return {
     'alexghergh/nvim-tmux-navigation',
     config = function()
       require('nvim-tmux-navigation').setup { disable_when_zoomed = true }
-      vim.keymap.set('n', '<C-h>', require('nvim-tmux-navigation').NvimTmuxNavigateLeft, { desc = 'Switch window left' })
-      vim.keymap.set('n', '<C-j>', require('nvim-tmux-navigation').NvimTmuxNavigateDown, { desc = 'Switch window down' })
-      vim.keymap.set('n', '<C-k>', require('nvim-tmux-navigation').NvimTmuxNavigateUp, { desc = 'Switch window up' })
-      vim.keymap.set('n', '<C-l>', require('nvim-tmux-navigation').NvimTmuxNavigateRight, { desc = 'Switch window right' })
+      vim.keymap.set(
+        'n',
+        '<C-h>',
+        require('nvim-tmux-navigation').NvimTmuxNavigateLeft,
+        { desc = 'Switch window left' }
+      )
+      vim.keymap.set(
+        'n',
+        '<C-j>',
+        require('nvim-tmux-navigation').NvimTmuxNavigateDown,
+        { desc = 'Switch window down' }
+      )
+      vim.keymap.set(
+        'n',
+        '<C-k>',
+        require('nvim-tmux-navigation').NvimTmuxNavigateUp,
+        { desc = 'Switch window up' }
+      )
+      vim.keymap.set(
+        'n',
+        '<C-l>',
+        require('nvim-tmux-navigation').NvimTmuxNavigateRight,
+        { desc = 'Switch window right' }
+      )
     end,
     lazy = false,
   },
-  { 'folke/lazydev.nvim', ft = 'lua', opts = {
-    library = {
-      { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
     },
-  } },
+  },
   { 'Bilal2453/luvit-meta', lazy = true },
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
   'nvim-treesitter/nvim-treesitter-context',
+  {
+    'ap/vim-buftabline',
+    config = function()
+      vim.keymap.set('n', '<Tab>', ':bnext<CR>')
+      vim.keymap.set('n', '<S-Tab>', ':bprev<CR>')
+      vim.keymap.set('n', '<leader>x', ':bd<CR>')
+    end,
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,7 +11,12 @@ return {
     { '<C-n>', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
   },
   config = function()
-    vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { 'NeoTreeNormal' })
-    vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { 'NeoTreeNormalNC' })
+    vim.g.transparent_groups =
+      vim.list_extend(vim.g.transparent_groups or {}, { 'NeoTreeNormal' })
+    vim.g.transparent_groups =
+      vim.list_extend(vim.g.transparent_groups or {}, { 'NeoTreeNormalNC' })
   end,
+  opts = {
+    close_if_last_window = true,
+  },
 }
