9734f1a54dd5c81bafacbe8bdeef3fe6d9929a82
marea <m.arevalo.aragon@gmail.com>
2025-01-05 01:53:29 -0600

    Added neoconf

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,37 +5,86 @@ return {
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     'nvim-java/nvim-java',
-    { 'j-hui/fidget.nvim', opts = {
-      notification = {
-        window = {
-          winblend = 0,
+    'folke/neoconf.nvim',
+    {
+      'j-hui/fidget.nvim',
+      opts = {
+        notification = {
+          window = {
+            winblend = 0,
+          },
         },
       },
-    } },
+    },
     'hrsh7th/cmp-nvim-lsp',
   },
   config = function()
     vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+      group = vim.api.nvim_create_augroup(
+        'kickstart-lsp-attach',
+        { clear = true }
+      ),
       callback = function(event)
         local map = function(keys, func, desc, mode)
           mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          vim.keymap.set(
+            mode,
+            keys,
+            func,
+            { buffer = event.buf, desc = 'LSP: ' .. desc }
+          )
         end
 
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map(
+          'gd',
+          require('telescope.builtin').lsp_definitions,
+          '[G]oto [D]efinition'
+        )
+        map(
+          'gr',
+          require('telescope.builtin').lsp_references,
+          '[G]oto [R]eferences'
+        )
+        map(
+          'gI',
+          require('telescope.builtin').lsp_implementations,
+          '[G]oto [I]mplementation'
+        )
+        map(
+          '<leader>D',
+          require('telescope.builtin').lsp_type_definitions,
+          'Type [D]efinition'
+        )
+        map(
+          '<leader>ds',
+          require('telescope.builtin').lsp_document_symbols,
+          '[D]ocument [S]ymbols'
+        )
+        map(
+          '<leader>ws',
+          require('telescope.builtin').lsp_dynamic_workspace_symbols,
+          '[W]orkspace [S]ymbols'
+        )
         map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+        map(
+          '<leader>ca',
+          vim.lsp.buf.code_action,
+          '[C]ode [A]ction',
+          { 'n', 'x' }
+        )
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+        if
+          client
+          and client.supports_method(
+            vim.lsp.protocol.Methods.textDocument_documentHighlight
+          )
+        then
+          local highlight_augroup = vim.api.nvim_create_augroup(
+            'kickstart-lsp-highlight',
+            { clear = false }
+          )
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
             group = highlight_augroup,
@@ -49,17 +98,30 @@ return {
           })
 
           vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+            group = vim.api.nvim_create_augroup(
+              'kickstart-lsp-detach',
+              { clear = true }
+            ),
             callback = function(event2)
               vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+              vim.api.nvim_clear_autocmds {
+                group = 'kickstart-lsp-highlight',
+                buffer = event2.buf,
+              }
             end,
           })
         end
 
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+        if
+          client
+          and client.supports_method(
+            vim.lsp.protocol.Methods.textDocument_inlayHint
+          )
+        then
           map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            vim.lsp.inlay_hint.enable(
+              not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }
+            )
           end, '[T]oggle Inlay [H]ints')
         end
       end,
@@ -75,7 +137,11 @@ return {
     end
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    capabilities = vim.tbl_deep_extend(
+      'force',
+      capabilities,
+      require('cmp_nvim_lsp').default_capabilities()
+    )
 
     -- Enable the following language servers, They will automatically be installed.
     -- Available keys are:
@@ -87,7 +153,7 @@ return {
     local servers = {
       -- clangd = {},
       gopls = {},
-      jdtls = {},
+      -- jdtls = {},
       rust_analyzer = {},
       ts_ls = {},
 
@@ -113,11 +179,18 @@ return {
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+    require('neoconf').setup {}
+
     require('mason-lspconfig').setup {
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          server.capabilities = vim.tbl_deep_extend(
+            'force',
+            {},
+            capabilities,
+            server.capabilities or {}
+          )
           require('lspconfig')[server_name].setup(server)
         end,
         jdtls = function()
