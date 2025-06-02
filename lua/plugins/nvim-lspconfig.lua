6184ec2f5102570f13fd2fb52702ec0ec29a762b
marea <m.arevalo.aragon@gmail.com>
2025-06-01 21:02:11 -0600

    feat: Added config for deno lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,17 @@
+local nvim_lsp = require("lspconfig")
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      ts_ls = {
+        root_dir = nvim_lsp.util.root_pattern("package.json"),
+        single_file_support = false,
+      },
+      denols = {
+        filetypes = { "typescript", "typescriptreact" },
+        root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+      },
       html = {
         filetypes = { "html", "templ" },
       },
