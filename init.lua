50a871e217a6ccf56b882f6581e6473089accc3a
marea <m.arevalo.aragon@gmail.com>
2025-11-29 05:36:18 -0600

    feat: Added fzf

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+local original_select = vim.ui.select
+vim.ui.select = function(...)
+  -- Force load fzf-lua on first call
+  require("lazy").load({ plugins = { "fzf-lua" } })
+  local opts = require("lazy.core.plugin").values(require("lazy.core.config").plugins["fzf-lua"], "opts", false)
+  require("fzf-lua").register_ui_select(opts.ui_select or nil)
+  -- Call the newly registered vim.ui.select
+  return vim.ui.select(...)
+end
+
 require("config.lazy")
