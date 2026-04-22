67124e89cc4b99a8565c22c952cc24a78738c146
marea <m.arevalo.aragon@gmail.com>
2026-04-21 18:49:48 -0600

    feat: Changed dashboard image location

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
         sections = {
           {
             section = "terminal",
-            cmd = "chafa ~/.config/nvim/dash.jpg --format symbols --symbols all --size 80x80 --stretch; sleep .1",
+            cmd = "chafa ~/.images/nvim-dash.jpg --format symbols --symbols all --size 80x80 --stretch; sleep .1",
             width = 80,
             height = 80,
             padding = 0,
