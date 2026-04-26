69fed6effa87b1389419684143674b61e5680b02
marea <m.arevalo.aragon@gmail.com>
2026-04-26 03:06:50 -0600

    feat: Added 'jk' keymap to exit terminal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("t", "jk", "<C-\\><C-n>")
 vim.keymap.set("n", "<leader>zz", function()
   vim.o.scrolloff = 999 - vim.o.scrolloff
 end, { desc = "Toggle scroll center" })
