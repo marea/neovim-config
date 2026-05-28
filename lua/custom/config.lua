96ad7fece6ac13b5567696472e853c1272797e17
marea <m.arevalo.aragon@gmail.com>
2026-05-27 20:24:56 -0600

    feat: Setup snacks

--- lua/custom/config.lua
+++ lua/custom/config.lua
@@ -64,6 +64,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
---
+++ lua/custom/snacks.lua
@@ -0,0 +1,42 @@
+local gh = require('utils').gh
+
+vim.pack.add { gh 'folke/snacks.nvim' }
+
+require('snacks').setup {
+  image = {
+    enabled = true,
+  },
+  dashboard = {
+    enabled = true,
+    preset = {
+      header = [[
+▄  ▄▓▄ ▄▓▄    ▄▓▓▄ █▄  ▄ ▄▓▄    ▄▓▓▄    ▄▓▓▄ █▄
+ ▓▒▀ ▓█▀ ▓█  ░▒▀  ▀▒░   ░▒  ▀  ░▒  ▒█  ░▒▀  ▀▒░
+ ▀░  ▀▀  ▀▀  ▄▄    ▄▀   ▀▄     ▄▄ ▀▀   ▄▄    ▄▀
+ ▒▄  ▒▓  ▒▓  ▀░▒▄ ▄░▒   ▒░▄    ▀░▒▄ ▄  ▀░▒▄ ▄░▒
+▀ ▀  ▀▀  ▀▀    ▀▀▀  ▀  ▀▀  ▀     ▀▀▀     ▀▀▀  ▀
+      ]],
+    },
+    sections = {
+      {
+        section = 'header',
+      },
+      {
+        pane = 2,
+        section = 'terminal',
+        cmd = 'chafa ~/.images/nvim-dash.jpg --format symbols --symbols all --size 60x60 --stretch; sleep .1',
+        width = 60,
+        height = 60,
+        padding = 0,
+      },
+      { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+      { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+      { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+    },
+  },
+  terminal = {
+    enabled = true,
+  },
+}
+
+vim.keymap.set({ 'n', 't' }, '<C-/>', Snacks.terminal.toggle)
